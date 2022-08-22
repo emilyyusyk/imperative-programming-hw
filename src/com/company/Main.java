a82745a9670248fa4dced3fb21bba5bfaa953ed3
emilyyu <yuhuayu.emily@gmail.com>
2022-08-22 09:47:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int A = 2;
+        int B = 2;
+
+        int s;
+        if (A == B)
+            s = (A + B) * (A + B);
+        else
+            s = A + B;
+
+        System.out.println (s);
+    }
+}
